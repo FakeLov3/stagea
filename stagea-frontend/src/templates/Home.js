8ef9928be6abf5987dc77d9a49de2ac1ec68bdf9
r-reis <rafa.reissilva@gmail.com>
2020-05-01 23:03:37 -0300

    feat(stagea-frontend): init react-app

---
+++ stagea-frontend/src/templates/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/css/home.css';
+
+export default function Home(){
+    return(
+        <div className="r_main">
+            <p>Hello World!</p>
+        </div>
+    );
+}
