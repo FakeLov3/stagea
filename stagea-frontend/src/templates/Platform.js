b1861ca105f60ab0df2ba8cf4e8b450bc362c870
r-reis <rafa.reissilva@gmail.com>
2020-05-03 05:22:53 -0300

    feat(Schedule): New platform component

--- stagea-frontend/src/templates/Platform.js
+++ stagea-frontend/src/templates/Platform.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Rooms from '../components/Rooms';
-import '../static/styles/css/rooms.css';
+import VMaps from '../components/VirtualMap';
+import '../static/styles/css/platform.css';
 
 export default function Platform() {
     return (
         <div className="platform">
             <Rooms/>
+            <VMaps/>
         </div>
     );
 };
