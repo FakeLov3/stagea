dc45f8c6f7dec681faffd0c8a88d9e02adf0b339
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2020-05-04 20:49:57 -0300

    chore(platform): adjusted Stage page

--- stagea-frontend/src/templates/platform/Stage.js
+++ stagea-frontend/src/templates/platform/Stage.js
@@ -10,7 +10,7 @@ import {
 import Rooms from "../../components/Rooms";
 import NetworkingDrawer from "../../components/NetworkingDrawer";
 import { makeStyles } from "@material-ui/core/styles";
-import speakerVideoImage from 'static/images/stage/speaker_video.png';
+import speakerVideoImage from "static/images/stage/speaker_video.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,85 +36,77 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "0.7em",
   },
   img: {
-    width: "100%"
-  }
+    width: "100%",
+  },
 }));
 
 function StageFragment() {
   const classes = useStyles();
 
   return (
-    <Grid container spacing={3} className={classes.container}>
-      <Grid container className={classes.titleContainer}>
+    <Grid container spacing={3} className={classes.container} justify="center">
+      <Grid className={classes.titleContainer} xs={10}>
         <Typography variant="h5">PEGN Expo 2020</Typography>
       </Grid>
-      <Grid container justify="center">
-          <img className={classes.img} src={speakerVideoImage} />
+
+      <Grid item xs={10}>
+        <img className={classes.img} src={speakerVideoImage} />
       </Grid>
 
-      <Grid container spacing={8}>
-        <Grid item xs={6}>
-          <Card>
-            <CardContent>
-              <Typography>Learn Figma</Typography>
-              <Box>
-                <img src='' />
-                <Typography variant="body1">UX Designer</Typography>
-                <Typography variant="body2">Stagea</Typography>
-              </Box>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardContent>
-              <Typography>Matthew Takenesse</Typography>
-              <Box>
-                <img src='' />
-                <Typography variant="body1">From NY</Typography>
-                <Typography variant="body2">takenessma.co</Typography>
-              </Box>
-            </CardContent>
-          </Card>
-        </Grid>
+      <Grid item xs={5}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography>Learn Figma</Typography>
+            <Box>
+              <img src="" />
+              <Typography variant="body1">UX Designer</Typography>
+              <Typography variant="body2">Stagea</Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={5}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography>Matthew Takenesse</Typography>
+            <Box>
+              <img src="" />
+              <Typography variant="body1">From NY</Typography>
+              <Typography variant="body2">takenessma.co</Typography>
+            </Box>
+          </CardContent>
+        </Card>
       </Grid>
 
-      <Grid container spacing={8}>
-        <Grid item xs={12}>
-          <Card>
-            <CardMedia></CardMedia>
-            <CardContent>
-              <Typography variant="body1">PDF/Figma kit</Typography>
-              <Typography variant="body2">450mb</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+      <Grid item xs={10}>
+        <Card className={classes.card}>
+          <CardMedia></CardMedia>
+          <CardContent>
+            <Typography variant="body1">PDF/Figma kit</Typography>
+            <Typography variant="body2">450mb</Typography>
+          </CardContent>
+        </Card>
       </Grid>
 
-      <Grid container spacing={8}>
-        <Grid item xs={12}>
-          <Card>
-            <CardMedia></CardMedia>
-            <CardContent>
-              <Typography variant="body1">Slides/Figma</Typography>
-              <Typography variant="body2">150mb</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+      <Grid item xs={10}>
+        <Card className={classes.card}>
+          <CardMedia></CardMedia>
+          <CardContent>
+            <Typography variant="body1">Slides/Figma</Typography>
+            <Typography variant="body2">150mb</Typography>
+          </CardContent>
+        </Card>
       </Grid>
 
-      <Grid container spacing={8}>
-        <Grid item xs={12}>
-          <Card>
-            <CardMedia></CardMedia>
-            <CardContent>
-              <Typography variant="body1">Invite me to chat</Typography>
-              <Typography variant="body2">matthew@stagea.com</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+      <Grid item xs={10}>
+        <Card className={classes.card}>
+          <CardMedia></CardMedia>
+          <CardContent>
+            <Typography variant="body1">Invite me to chat</Typography>
+            <Typography variant="body2">matthew@stagea.com</Typography>
+          </CardContent>
+        </Card>
       </Grid>
-
     </Grid>
   );
 }
