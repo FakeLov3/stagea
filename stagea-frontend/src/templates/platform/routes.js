729532ce530ac7891138a4a921953ab10cef836b
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2020-05-04 22:44:50 -0300

    fix(platform): reuse Platform component

--- stagea-frontend/src/templates/platform/routes.js
+++ stagea-frontend/src/templates/platform/routes.js
@@ -7,17 +7,46 @@ import Connect from "./Connect";
 import Press from "./Press";
 import Sponsors from "./Sponsors";
 import Support from "./Support";
+import Lobby from "./Lobby";
 
 export default function PlatformRoutes() {
   return (
     <>
-      <Route path="/platform" exact component={Platform} />
-      <Route path="/platform/stage" exact component={Stage} />
-      <Route path="/platform/sessions" exact component={Sessions} />
-      <Route path="/platform/connect" exact component={Connect} />
-      <Route path="/platform/press" exact component={Press} />
-      <Route path="/platform/sponsors" exact component={Sponsors} />
-      <Route path="/platform/support" exact component={Support} />
+      <Route
+        path="/platform"
+        exact
+        component={() => <Platform child={<Lobby />} />}
+      />
+      <Route
+        path="/platform/stage"
+        exact
+        component={() => <Platform child={<Stage />} />}
+      />
+      <Route
+        path="/platform/sessions"
+        exact
+        component={() => <Platform child={<Sessions />} />}
+      />
+      <Route
+        path="/platform/connect"
+        exact
+        component={() => <Platform child={<Connect />} />}
+      />
+      <Route
+        path="/platform/press"
+        exact
+        component={() => <Platform child={<Press />} />}
+      />
+      <Route
+        path="/platform/sponsors"
+        exact
+        component={() => <Platform child={<Sponsors />} />}
+      />
+      <Route
+        path="/platform/support"
+        exact
+        component={() => <Platform child={<Support />} />}
+      />
     </>
   );
 }
